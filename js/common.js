ead5129a67ae6e5632ea0e184da0b3b97eec12b6
Ihor <ihor_cherednyk@mail.ru>
2016-06-22 15:50:15 +0300

    bobs

--- js/common.js
+++ js/common.js
@@ -9,21 +9,37 @@ $(function() {
 	});
 
 
+
 	$(".l-anim").animated("fadeInLeft");
 	$(".welcome-content").animated("fadeInLeft");
 	$(".c-anim").animated("zoomIn");
 	$(".r-anim").animated("fadeInRight");
 	$(".welcome-aside").animated("fadeInRight");
 
+	// $(window).resize(function(){
+		
+		$(".products-info li a").on("click", function(e){
+			e.preventDefault();
+			if(window.innerWidth < 480){
+				console.log(window.innerWidth)
+				var p = $(this).find("p");
+				p.stop().toggle(600);
+			}else if(window.innerWidth < 480){
+				$(".products-info li a").off("click")
+				p.css("display", "block")
+			}
+		});
 
-	$(".toggle_mnu").click(function() {
-		if ($(".sandwich").hasClass("active")) {
-			$(".main-mnu ul").stop().fadeOut(600);
-			$(".sandwich").toggleClass("active");
-		} else {
-			$(".main-mnu ul").stop().fadeIn(600);
-			$(".sandwich").toggleClass("active");
-		};
-	});
+	// });
+
+$(".toggle_mnu").click(function() {
+	if ($(".sandwich").hasClass("active")) {
+		$(".main-mnu ul").stop().fadeOut(600);
+		$(".sandwich").toggleClass("active");
+	} else {
+		$(".main-mnu ul").stop().fadeIn(600);
+		$(".sandwich").toggleClass("active");
+	};
+});
 
 });
